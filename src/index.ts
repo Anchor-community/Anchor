f3c8971393c62542ff62f07780353ca9d19e9055
Hirokazu Degawa <hxdegawa@gmail.com>
2020-10-07 19:10:40 +0900

    Implement all

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { Client, Guild, Role, TextChannel } from 'discord.js'
+import { config } from 'dotenv'
+
+config()
+
+const client = new Client()
+const TOKEN = process.env.BOT_TOKEN
+
+interface Channels {
+  [key: string]: TextChannel | undefined
+}
+
+const channels: Channels = {
+  general: undefined,
+  introduce: undefined,
+  announce: undefined,
+  random: undefined,
+  events: undefined,
+  ideas: undefined,
+}
+
+interface Roles {
+  [key: string]: Role | undefined
+}
+
+const roles: Roles = {
+  guest: undefined,
+  verified: undefined,
+}
+
+client.on('ready', () => {
+  const server: Guild | undefined = client.guilds.cache.find(
+    (guild) => guild.name === 'Anchor'
+  )
+
+  server?.channels.cache.forEach((channel: any) => {
+    if (channel.name in channels) channels[channel.name] = channel
+  })
+
+  server?.roles.cache.forEach((role: Role) => {
+    if (role.name.toLocaleLowerCase() in roles)
+      roles[role.name.toLocaleLowerCase()] = role
+  })
+})
+
+client.on('message', (message) => {
+  if (message.author.bot) return
+
+  if (message.channel === channels.introduce) {
+    if (!message.member?.roles.cache.find((role) => role === roles.guest))
+      return
+
+    message.member?.roles
+      .remove(roles.guest as Role)
+      .then((member) => member.roles.add(roles.verified as Role))
+  }
+})
+
+client.on('guildMemberAdd', (member) => {
+  member.roles.add(roles.guest as Role)
+})
+
+client.login(TOKEN)
