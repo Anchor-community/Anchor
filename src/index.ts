675ba3816602530d079116c57e7d0ee57c94da64
kokoa0429 <kokoa@ciebus.net>
2020-12-01 19:46:37 +0900

    add /title command

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { config } from 'dotenv'
-import { Client, Emoji, Guild, GuildChannel, Role } from 'discord.js'
+import { Client, Guild, GuildChannel, Role, Snowflake } from 'discord.js'
+import { setTimeout } from 'timers'
 
 config()
 
 export let server: Guild | undefined = undefined
 
-const TOKEN = process.env.BOT_TOKEN
+// const TOKEN = process.env.BOT_TOKEN
 const client = new Client()
 
 interface ChannelID {
@@ -49,6 +50,7 @@ interface StreakCounter {
 }
 
 const streak: StreakCounter = {}
+let vcChangeCount: Map<Snowflake,number> = new Map()
 
 client.on('ready', async () => {
   server = await client.guilds.cache.find((guild) => guild.name === 'Anchor')
@@ -121,26 +123,56 @@ client.on('message', (message) => {
   }
   
   // コマンド
-  if(message.member?.roles.cache.has(roleIDs.verified)) {
+  // ウンコード
+  if(message.member?.roles.cache.has("782484108802523138")) {
     if(message.content.startsWith("ping")) message.channel.send("pong")
     if(message.content.startsWith("/")) {
       const args: string[] = message.content.slice(1).trim().split(/ +/)
       switch(args[0]) {
         case 'title':
-          if(!args[1] || args[3]) {
+          if(!args[1] || args[2]) {
             message.channel.send({
               embed: {
               color: 16757683,
-              title: ":information_source:使い方",
-              description: '`/title [name]` で入っているVCのタイトルを変更できます'
-            }})
+              title: ":information_source: 使い方",
+              description: '`/title [name]` で入っているVCのタイトルを変更できます\n'+
+                'このサーバのステータスは ' 
+                + (vcChangeCount.get(message.guild?.id!) as number >= 2 
+                  ? "レートリミットにかかっている可能性があります。" 
+                  : "大丈夫です。") 
+            }}).then(msg => setTimeout(() => {msg.delete(); message.delete()},5000))
             return
           }
-          message.guild?.channels.cache.filter(c => c.type == 'voice')
+
+          const cnl: GuildChannel | undefined = message.guild?.channels.cache.filter(c => c.type == 'voice')
             .filter(c => !!c.members.get(message.author.id))
             .first()
-            ?.setName(args[2])
-            .then(async() => message.react('☑'))
+
+            if(vcChangeCount.get(message.guild?.id!) as number >= 2){
+              message.channel.send("レートリミットにかかっている可能性があります").then(msg => setTimeout(() => {msg.delete(); message.delete()},5000))
+              return
+            }
+
+            cnl?.setName(args[1])
+            .then(async() => {
+              message.react('☑')
+              setTimeout(()=> {
+                message.deletable ? message.delete() : {}
+              },3000)
+              //ウンコード
+              !vcChangeCount.has(message.guild?.id!) ? 
+                vcChangeCount.set(message.guild?.id!, 1)
+                : vcChangeCount.set(message.guild?.id!, vcChangeCount.get(message.guild?.id!) as number + 1)
+              setTimeout(() => {
+                vcChangeCount.get(message.guild?.id!) == 1
+                  ? vcChangeCount.delete(message.guild?.id!)
+                  : vcChangeCount.set(message.guild?.id!, vcChangeCount.get(message.guild?.id!) as number - 1)
+              }, 5 * 60 * 1000)
+            })
+            .catch((error) => {
+              message.channel.send("チャンネル名の変更に失敗しました。")
+              console.error(error)
+            })
       }
     }
   }
@@ -154,4 +186,4 @@ client.on('guildMemberAdd', (member) => {
 
 client.on('error', console.error)
 
-client.login(TOKEN)
+client.login("NzgyNTgyMTY3NjUzMDU2NTQy.X8OScA.K-g_u5dj8XmU4VvunFRgLFiC69Y")
